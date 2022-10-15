589255d43c5f32a3884d3d5a6c57a36b4455ab0f
Tatiana <tt_tanya_88@mail.ru>
2022-10-15 13:53:36 +0300

    z4

---
+++ z4/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая выводит массив из 8 элементов,
+// заполненный нулями и единицами в случайном порядке
+
+int [] array = new int [8];
+Random rnd = new Random();
+for ( int i = 0; i < array.Length; i++)
+{
+    array[i] = rnd.Next(0,2);
+    Console.WriteLine(array[i]);
+    i++;
+}
