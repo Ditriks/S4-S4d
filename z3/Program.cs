b3b40158387217689b57731a023613a5d39acf60
Tatiana <tt_tanya_88@mail.ru>
2022-10-15 13:53:23 +0300

    z3

---
+++ z3/Program.cs
@@ -0,0 +1,15 @@
+﻿// напишите программу, которая принимает число N и выдает 
+// произведение чисел от 1 до N
+
+Console.Write("Введите любое число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+int res = 1;
+
+while (i <= x)
+{
+    res = res * i;
+    i++;
+}
+
+Console.WriteLine(res);
