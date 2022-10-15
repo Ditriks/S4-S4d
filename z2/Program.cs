dff678b26eae0219f4573c0cfcf9bf8bbae37607
Tatiana <tt_tanya_88@mail.ru>
2022-10-15 13:53:07 +0300

    z2

---
+++ z2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число
+// и выдает количество цифр в числе
+
+Console.Write("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+int Dl = 0;
+
+while (x >= 1)
+{
+    x = x/10;
+    Dl++;
+}
+
+Console.WriteLine(Dl);
