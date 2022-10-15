52c35169559dcf607bdc9fe3610f62ef48953a2c
Tatiana <tt_tanya_88@mail.ru>
2022-10-15 13:52:38 +0300

    z1

---
+++ z1/Program.cs
@@ -0,0 +1,14 @@
+﻿// напишите программу, которая принимает число N и выдает сумму чисел от 1 до N
+
+Console.Write("Введите любое число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+int res = 0;
+
+while (i <= x)
+{
+    res = res + i;
+    i++;
+}
+
+Console.WriteLine(res);
