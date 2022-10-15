f30003f95c6e689f7cecd1b82dcf11e18dde5805
Tatiana <tt_tanya_88@mail.ru>
2022-10-15 13:54:28 +0300

    dz2

---
+++ dz2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int Sum(int N){
+    
+    int counter = Convert.ToString(N).Length;
+    int advance = 0;
+    int result = 0;
+
+    for (int i = 0; i < counter; i++){
+      advance = N - N % 10;
+      result = result + (N - advance);
+      N = N / 10;
+    }
+   return result;
+}
+
+int sum = Sum(N);
+
+Console.WriteLine("Сумма всех цифр: " + sum);
